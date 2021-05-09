0f757ecc18a20e9ee6717eb0c1e87727c99f0d6f
João Balardin <joao_carpes@hotmail.com>
2021-05-09 01:20:07 -0300

    login pronto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
+import 'dart:developer';
+
 import 'package:finalproject/app/app_module.dart';
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
-  runApp(AppModule());
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          log("Deu Erro");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppModule();
+        }
+
+        return MaterialApp(
+          title: 'Flutter Slidy',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: Container(
+            child: Text("Carregando"),
+          ),
+        );
+      },
+    );
+  }
 }
